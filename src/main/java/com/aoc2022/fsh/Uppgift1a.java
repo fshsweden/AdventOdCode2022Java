1f5973f2d7a350d56576f983d00c9b891565a183
Peter Andersson <fshsweden@gmail.com>
2022-12-04 13:29:18 +0100

    Initial commit of Java version

---
+++ src/main/java/com/aoc2022/fsh/Uppgift1a.java
@@ -0,0 +1,108 @@
+package com.aoc2022.fsh;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/*
+# calculate sum of numbers in lines array until blank line found
+def sum_numbers(lines):
+sums = []
+sums.append(0)
+index=0
+for line in lines:
+if line == '' or line == '\n':
+sums.append(0)
+index += 1
+else:
+sums[index] += int(line)
+return sums
+
+sums = sum_numbers(lines)
+
+print(max(sums))
+*/
+
+
+public class Uppgift1a {
+
+    public static int sum_numbers(List<String> lines) {
+        int[] sums = new int[lines.size()];
+        int index = 0;
+        for (String line : lines) {
+            if (line.equals("") || line.equals("\n")) {
+                index += 1;
+            } else {
+                sums[index] += Integer.parseInt(line);
+            }
+        }
+        return Arrays.stream(sums).sum();
+    }
+
+    public static List<Integer> solve(List<String> data) {
+        List<Integer> calories = new ArrayList<>();
+        List<String> tmp = new ArrayList<>();
+        for (String str: data) {
+            if (str.equals("") || str.equals("\n")) {
+                // System.out.println(tmp + " " + sum_numbers(tmp));
+                calories.add(sum_numbers(tmp));
+                tmp.clear();
+            } else {
+                tmp.add(str);
+            }
+        }
+        return calories;
+    }
+
+    public static void main(String[] args) {
+
+        List<String> testdata = new ArrayList<>();
+        testdata.add("1000");
+        testdata.add("2000");
+        testdata.add("3000");
+        testdata.add("");
+        testdata.add("4000");
+        testdata.add("");
+        testdata.add("5000");
+        testdata.add("6000");
+        testdata.add("");
+        testdata.add("7000");
+        testdata.add("8000");
+        testdata.add("9000");
+        testdata.add("");
+        testdata.add("10000");
+
+        String filename = "src/main/resources/input-1.txt";
+
+        try (Stream<String> lines = Files.lines(Paths.get(filename))) {
+
+            List<String> data = lines.collect(Collectors.toList());
+
+            List<Integer> calories = solve(data);
+            System.out.println(calories.stream().max(Integer::compare).orElse(0));
+
+            List<Integer> calories2 = solve(testdata);
+            System.out.println(calories2.stream().max(Integer::compare).orElse(0));
+
+            /*
+            // Stupid solution that joins all items into one large String
+            // System.out.println(data.stream().collect(Collectors.joining("\n")).split("\n{2}"));
+
+            Arrays.stream(data.stream().collect(Collectors.joining("\n")).split("\n{2}"))
+                    .map(s -> Arrays.stream(s.split("\n")).collect(Collectors.toList()))
+                    .collect(Collectors.toList())
+                    .forEach(System.out::println);
+
+            data.stream().forEach(System.out::println);
+*/
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
